13e97dd032ae2750048f612d513bd6c1917e43a3
jaakkosh <jaakko.hotti@metropolia.fi>
2020-09-04 14:03:06 +0300

    kopio

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navigator from './navigators/Navigator';
+
+const App = () => {
+  return (
+    <Navigator />
+  );
+};
+
+export default App;
---
+++ components/ListItem.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
+
+const ListItem = ({navigation, singleMedia}) => {
+  return (
+    <TouchableOpacity style={styles.row} onPress={
+      () => {
+        navigation.navigate('Single', {file: singleMedia});
+      }
+    }>
+      <View style={styles.imagebox}>
+        <Image
+          style={styles.image}
+          source={{uri: mediaUrl + singleMedia.thumbnails.w160}}
+        />
+      </View>
+      <View style={styles.textbox}>
+        <Text style={styles.listTitle}>{singleMedia.title}</Text>
+        <Text>{singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: '#eee',
+    borderRadius: 16,
+  },
+  imagebox: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    borderRadius: 16,
+  },
+  textbox: {
+    flex: 2,
+    padding: 10,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+});
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+  navigation: PropTypes.object,
+};
+
+export default ListItem;
